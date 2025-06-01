3006d25e0e0fbe198672264b8857bfe163d94e32
Johel H <johel.hered@gmail.com>
2025-06-01 00:49:38 -0600

    add the input and search tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import{ useState,useEffect } from 'react';
+import{ useState,useEffect,useRef, useMemo } from 'react'; //se importaron los hooks necesarios
 import './App.css'
 import axios from 'axios';
 import InsumosCard from './components/InsumosCard';
@@ -23,30 +23,49 @@ function App() {
   const [insumosData, setInsumos] = useState<Insumo[]>([]);
   const [Loading,setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>('');//<- estado para la busqueda
+  const searchInputRef = useRef<HTMLInputElement>(null);//referencia para el input de busqueda
 
   useEffect(() => {
     const fetchInsumos = async (): Promise<void> => {
       try {
         const response = await axios.get<Insumo[]>('https://fakestoreapi.com/products');
         setInsumos(response.data);
-        setLoading(false);
+        /* setLoading(false); */ //Colocado en el finally para asegurar que se ejecute siempre
 
       }
       catch (error) {
         if (error instanceof Error) {
-          setLoading(false);
+          /* setLoading(false); */ // Colocado en el finally para asegurar que se ejecute siempre
           setError(error.message);
         } else {
-          setLoading(false);
+          /* setLoading(false); */ // Colocado en el finally para asegurar que se ejecute siempre 
           setError('Error al cargar los productos');
         }
         console.log('Error al consultar los productos');
+      }
+      finally {
+        setLoading(false); //Se ejecuta siempre, independientemente de si hubo error o no 
       }
     }
     fetchInsumos();
   }, []);
 
-  console.log(insumosData);
+  //los usamos para poner el foco en el input *REVISAR*
+  useEffect(() => {
+    searchInputRef.current?.focus();
+  },[])
+
+  //Lo usamos para filtrar eficientemente los productos según el término de búsqueda
+  const filteredInsumos = useMemo(() => {
+    console.log("Filtrando insumos...");
+    return insumosData.filter((insumo) =>
+      insumo.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      insumo.description.toLowerCase().includes(searchTerm.toLowerCase()) 
+    );
+  }, [insumosData, searchTerm]); //<- dependencias - se recalcula solo si insumosData o searchTerm cambian
+
+  /* console.log(insumosData); */
 
   if(Loading){
     return (<div className="flex justify-center items-center h-screen"><div className="loader"></div></div>)
@@ -56,10 +75,19 @@ function App() {
   }
 
   return (
-    <>
-      <h1>Proyecto Final</h1>
-      <ProductsList insumos={insumosData} />
-    </>
+    <div className="p-4 container mx-auto">
+        <h1 className="text-4xl font-bold text-center my-6 text-gray-900">Lista de Productos</h1>
+        <div className="mb-6 flex justify-center">
+          <input type="text" className="border border-gray-300 rounded-lg p-2 w-full max-w-lg shadow-lg focus:outline-none focus:ring-2 
+          focus:ring-blue-500" 
+          placeholder="Buscar productos..."
+          ref={searchInputRef}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      <ProductsList insumos={filteredInsumos} />
+    </div>
   )
 }
 
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -8,10 +8,9 @@ interface ProductsListProps {
 const ProductsList = ({ insumos }: ProductsListProps) => {
     return (
     (<div className="p-4">
-        <h1 className="text-3xl font-bold text-center mb-6">Lista de Productos</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {insumos.map((insumo:Insumo) =>(
-                <InsumosCard insumo={insumo}/>
+                <InsumosCard key={insumo.id} insumo={insumo}/>
             ))}
         </div>
     </div>)
